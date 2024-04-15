8e8fe491ea900b1c9e4d26b17cca98597d6b4a18
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-04-15 01:03:38 -0400

    Added descirption for types of algorithms

--- Lesson_1_Algorithms/Program.cs
+++ Lesson_1_Algorithms/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+1. Константные (Constant): Algorithms that take the same amount of time regardless of the size of the input. In Big O notation, these are denoted as O(1).
+2. Логарифмические (Logarithmic): Algorithms whose time complexity grows logarithmically with the size of the input. Examples include binary search. Denoted as O(log n).
+3. Линейные (Linear): Algorithms where the time taken increases linearly with the size of the input. Denoted as O(n).
+4. Линейно-логарифмические (Linearithmic): Algorithms with time complexity that is a combination of linear and logarithmic growth. Denoted as O(n log n).
+5. Квадратные (Quadratic): Algorithms whose time complexity grows quadratically with the size of the input. Examples include nested loops where each loop iterates over the size of the input. Denoted as O(n^2).
+6. Кубические (Cubic): Algorithms with time complexity that grows cubically with the size of the input. Denoted as O(n^3).
+*/
