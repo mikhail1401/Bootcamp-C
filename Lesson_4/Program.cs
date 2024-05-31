bec4b26e338a24d3120b9a9422a6383b3cefa61a
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-05-30 23:03:44 -0400

    Started Lesson 4 - Bubble Sort

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Bubble sort - Пузырьковая сортировка
+
+int n = 5;
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = Random.Shared.Next(10);
+}
+
+Console.WriteLine($"[{String.Join(', ', array)}]");
