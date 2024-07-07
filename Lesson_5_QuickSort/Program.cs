94620a770c41e061c231d8e1aaa4369597119f98
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-07-07 17:05:05 -0400

    Added quickSort method in Lesson 5

--- Lesson_5_QuickSort/Program.cs
+++ Lesson_5_QuickSort/Program.cs
@@ -13,7 +13,7 @@ int sumNumbers(int x, int y)
 Console.WriteLine($"Result is {numbers[0]} + {numbers[1]} = {sumNumbers(numbers[0], numbers[1])}");
 
 
-// [7, 4, 1, 3, 5, 2, 6] - [1, 2, 3, 4, 5, 6, 7]
+// [7, 4, 1, 3, 5, 2, 6] -> [1, 2, 3, 4, 5, 6, 7]
 // pivot = 7 - берется опорный элемент
 // массив с элементами меньше опорного + опорный элемент + массив с элементаи больше или равными опорному
 // [4, 1, 3, 5, 2, 6] + [7] + []
@@ -28,10 +28,40 @@ Console.WriteLine($"Result is {numbers[0]} + {numbers[1]} = {sumNumbers(numbers[
 // [2] + [3] + []
 
 
-int[] inputArray(int[] array)
+void inputArray(int[] array)
 {
-    for (int i = 0; i < array.GetLength; i++)
-        array[i] = new Random().Next(-20; 21);
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);
+}
+
+int[] quickSort(int[] array, int leftIndex, int rightIndex)
+{
+    int i = leftIndex, j = rightIndex, pivot = array[leftIndex];
+    while (i <= j)
+    {
+        while (array[i] < pivot)
+        {
+            i++;
+        }
+        while (array[j] > pivot)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (leftIndex < j)
+        quickSort(array, leftIndex, j);
+
+    if (i < rightIndex)
+        quickSort(array, i, rightIndex);
+
     return array;
 }
 
@@ -39,4 +69,6 @@ Console.Write("Enter number of elements in the array: ");
 int n = int.Parse(Console.ReadLine()!);
 int[] array = new int[n];
 
-// From 24:30
+inputArray(array);
+Console.WriteLine($"Initial array: [{string.Join(", ", array)}]");
+Console.WriteLine($"Final array: [{string.Join(", ", quickSort(array, 0, array.Length - 1))}]");
