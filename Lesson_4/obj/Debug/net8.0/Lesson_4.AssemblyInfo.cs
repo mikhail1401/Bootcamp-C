846d3f7ef4f8fa1514c0dc5c49a401e5c4eb9217
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-07-13 22:19:40 -0400

    Debuggin cache?

--- Lesson_4/obj/Debug/net8.0/Lesson_4.AssemblyInfo.cs
+++ Lesson_4/obj/Debug/net8.0/Lesson_4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee322af82ba282c41bdc0480db7b3894a6fd4bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lesson_5_QuickSort/obj/Debug/net8.0/Lesson_5_QuickSort.AssemblyInfo.cs
+++ Lesson_5_QuickSort/obj/Debug/net8.0/Lesson_5_QuickSort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson_5_QuickSort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee322af82ba282c41bdc0480db7b3894a6fd4bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23810a873784d0cbce203423090070c15db95f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson_5_QuickSort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson_5_QuickSort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
