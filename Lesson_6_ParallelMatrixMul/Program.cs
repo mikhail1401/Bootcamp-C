8bf947195a5264c4bbe4ba32f8610455d97d7670
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-07-23 00:34:23 -0400

    Created Lesson_6_ParallelMatrixMul

---
+++ Lesson_6_ParallelMatrixMul/Program.cs
@@ -0,0 +1,7 @@
+﻿const int N = 1000; // matrix size; const because this parameter will never change
+// const variables are written in CAPITAL letters (e.g. const int PI)
+
+int[,] serialMulRes = new int[N, N]; // результат выполнения умножения матриц в однопотоке
+int[,] threadMulRes = new int[N, N]; // результат параллельного умножения матриц
+
+
